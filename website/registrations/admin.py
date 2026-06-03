7b5568e54154b1f6115663be69f50bee36b640f2
Jens <jens.broeren@ru.nl>
2026-06-03 21:31:18 +0200

    fix for test_get_user_changelist_projectfilter

--- website/registrations/admin.py
+++ website/registrations/admin.py
@@ -303,7 +303,7 @@ class UserAdminProjectFilter(AutocompleteFilter):
     def queryset(self, request, queryset):
         """Filter out participants in the specified Project."""
         if self.value():
-            return queryset.filter(registrations__projects=self.value())
+            return queryset.filter(registrationsubmission__projects=self.value())
         return queryset
 
 
